d3cb3ea772962f1ddc8ab040bfb6e1d7cb670388
Savan Visalpara <savan77@users.noreply.github.com>
2020-07-13 13:22:52 -0500

    solve minor issues with models

--- research/object_detection/metrics/tf_example_parser.py
+++ research/object_detection/metrics/tf_example_parser.py
@@ -44,9 +44,12 @@ class StringParser(data_parser.DataToNumpyParser):
     self.field_name = field_name
 
   def parse(self, tf_example):
-    return "".join(tf_example.features.feature[self.field_name]
-                   .bytes_list.value) if tf_example.features.feature[
-                       self.field_name].HasField("bytes_list") else None
+    if tf_example.features.feature[self.field_name].HasField("bytes_list"):
+        result = tf_example.features.feature[self.field_name].bytes_list.value
+        result = "".join([x if type(x)=='str' else x.decode('utf-8') for x in result])
+    else:
+        result = None
+    return result
 
 
 class Int64Parser(data_parser.DataToNumpyParser):
